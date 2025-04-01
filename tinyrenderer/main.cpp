cf9f7afe9773fe4a66ab82c4a4df68bdd251fdf5
AnyingLW <2038470544@qq.com>
2025-04-01 10:58:26 +0800

    切线空间法线贴图

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -18,24 +18,43 @@ Vec3f up(0, 1, 0);
 
 struct GouraudShader :public IShader {
 	mat<2, 3, float>varying_uv;
-	mat<4, 4, float>uniform_M;
-	mat<4, 4, float>uniform_MIT;
+	mat<4, 3, float>varying_tri;
+	mat<3, 3, float>varying_nrm;
+	mat<3, 3, float>ndc_tri;
 
 	virtual Vec4f vertex(int iface, int nthvert) {//顶点着色器：计算屏幕坐标及uv坐标
 		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+		varying_nrm.set_col(nthvert, proj<3>((Projection * ModelView).invert_transpose() * embed<4>(model->normal(iface, nthvert))));
 		Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
+		varying_tri.set_col(nthvert, gl_Vertex);
+		ndc_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
 		return Viewport * Projection * ModelView * gl_Vertex;
 	}
 
-	virtual bool fragment(Vec3f bar, TGAColor& color) {//片段着色器：返回该点对应颜色
+	virtual bool fragment(Vec3f bar, TGAColor& color) {//片段着色器：返回该点对应颜色(应用切线空间法线贴图)
+		Vec3f bn = (varying_nrm * bar).normalize();
 		Vec2f uv = varying_uv * bar;
-		Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();//uv得到的法线*逆转置矩阵得到正确的法线
-		Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
-		Vec3f r = (n * (n * l * 2.f) - l).normalize();
-		float spec = pow(max(r.z, 0.f), model->specular(uv));
-		float diff = max(0.f, n * l);
-		color = model->diffuse(uv);
-		for (int i = 0; i < 3; i++)color[i] = min<float>(5 + color[i] * (diff + .6 * spec), 255);
+
+		mat<3, 3, float>A;
+		A[0] = ndc_tri.col(1) - ndc_tri.col(0);
+		A[1] = ndc_tri.col(2) - ndc_tri.col(0);
+		A[2] = bn;
+
+		mat<3, 3, float>AI = A.invert();//A的逆
+
+		Vec3f i = AI * Vec3f(varying_uv[0][1] - varying_uv[0][0], varying_uv[0][2] - varying_uv[0][0], 0);
+		Vec3f j = AI * Vec3f(varying_uv[1][1] - varying_uv[1][0], varying_uv[1][2] - varying_uv[1][0], 0);
+
+		mat<3, 3, float> B;
+		B.set_col(0, i.normalize());
+		B.set_col(1, j.normalize());
+		B.set_col(2, bn);
+
+		Vec3f n = (B * model->normal(uv)).normalize();
+
+		float diff = std::max(0.f, n * light_dir);
+		color = model->diffuse(uv)*diff;
+		
 		return false;
 	}
 };
@@ -46,13 +65,11 @@ int main() {
 	lookat(eye, center, up);
 	viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
 	projection(-1.f / (eye - center).norm());
-	light_dir.normalize();
+	light_dir = proj<3>((Projection * ModelView * embed<4>(light_dir, 0.f))).normalize();
 
 	TGAImage image(width, height, TGAImage::RGB);
 	TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
 	GouraudShader shader;
-	shader.uniform_M = Projection * ModelView;
-	shader.uniform_MIT = (Projection * ModelView).invert_transpose();
 	for (int i = 0; i < model->nfaces(); i++) {
 		Vec4f screen_coords[3];
 		for (int j = 0; j < 3; j++) {
